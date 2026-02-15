ff8a02a1756771cd95ab803819b65cc8050c700d
Chaal Pritam <chaalpritam@gmail.com>
2026-02-16 02:00:30 +0530

    feat: define TypeScript types for the entire domain model
    Port all Swift structs from the iOS app into TypeScript interfaces:
    City, User, UserKarma, KarmaLevel, Achievement, Wallet, Transaction,
    Cast, Comment, Poll, Task, Crowdfund, Tribe, TribeCategory, Subchannel,
    ExploreItem, ChatMessage, NearbyUser, MapEvent, UserProfile, UserStats,
    ActivityItem, NotificationItem, Conference, ConferenceEvent, Speaker, Venue.

---
+++ src/types/city.ts
@@ -0,0 +1,16 @@
+export interface Coordinates {
+  latitude: number;
+  longitude: number;
+}
+
+export interface City {
+  id: string;
+  name: string;
+  country: string;
+  countryCode: string;
+  imageUrl: string;
+  memberCount: number;
+  activeTribes: number;
+  coordinates: Coordinates;
+  accentColor: string;
+}
